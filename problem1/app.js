af7e6e6fa056b852c0a014fb6ad7c8a7b846791a
Nicolas <nicolasmayorano@gmail.com>
2022-06-01 15:50:19 -0300

    problem1 2.0

--- problem1/app.js
+++ problem1/app.js
@@ -1,9 +1,13 @@
 // Solución 1
 
-function suma(a, b) {
+function multiplicacion(a, b) {
   let acc = 0;
-  for (let i = 0; i < b; i++) {
-    acc += a;
+  for (let i = 0; i < Math.abs(b); i++) {
+    acc += Math.abs(a);
   }
+  if (a < 0 && b < 0) return acc;
+  if (a < 0 || b < 0) return -acc;
   return acc;
 }
+
+console.log(multiplicacion(5,5))
