87dc58e2e85ca8178d2ade3b627771f57f950be1
Nicolas <nicolasmayorano@gmail.com>
2022-06-04 10:27:28 -0300

    problem 5 y cambios en readme

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-### Instalación
+## Instalación
 
 ```js
 npm install
 ```
 
+## Visualización
+Para visualizar las soluciones propuestas tiene dos alternativas:
+### 1. Desde consola
+Debe ingresar al archivo correspondiente al problema de la carpeta "solutions".
 
-### Visualización
-
-##### 1. Desde consola
-Para visualizar los resultados a los problemas desde la consola debe ingresar al archivo correspondiente de la carpeta "solutions", quitar el comentario que precede a la invocación de console.log() ubicado al final del documento, colocar los argumentos que se entiendan adecuados para comprobar la correctitud de la solución propuesta y ejecutar en consola el comando ubicado en la línea de código siguiente.
-
-##### 2. Desde el navegador
-Para visualizar las soluciones propuestas desde el navegador web, corra en la consola el comando
+### 2. Desde el navegador
+Corra en la consola el comando
 ```js
 npm start
 ```
 Una vez inicializada la aplicacion, ingrese en el navegador a [localhost:3000](http://localhost:3000/). El puerto 3000 es determinado en la configuración por defecto y puede ser modificado al final de la sexta línea del archivo app.js de la raíz del proyecto.
-Una vez en el sitio, podrá navegar con los diferentes enlaces.
+
+## Ejecución
+### 1. Desde la consola
+Quitar el comentado que precede a la invocación de console.log() al final del documento, colocar los argumentos que se entiendan adecuados para comprobar la correctitud de la solución propuesta y ejecutar en consola el comando ubicado en la línea de código siguiente.
+
+### 2. Desde el navegador
+Una vez en el sitio, podrá navegar con los diferentes enlaces y completar los formularios con los valores que se entiendan adecuados para comprobar la correctitud de la solución propuesta.
--- public/problem5.js
+++ public/problem5.js
@@ -1,25 +1,59 @@
+// Helpers
+function debounce(func, delay) {
+  let timeout;
+  return (...args) => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
 function validator() {
-  const lis = document.getElementsByClassName('list-elem');
-  if (input.value){
-    
-    
+  const pwd = document.getElementById('password').value;
 
-
-
-
-    
-  } else {
+  if (!pwd) {
+    const lis = document.getElementsByClassName('list-elem');
     for (let elem of lis) {
       elem.style.color = 'black';
     }
+    return;
   }
+
+  let lowercase = 0, uppercase = 0, matchingAdjacentLetters = false, numbers = 0, matchingAdjacentNumbers = false;
+  let symbolsCount = 0, symbolNotUnique = false, adjacentSymbols = false, zero = 0, spaces = 0;    
+  let allowedSymbolsCharCodes = [33, 35, 36, 37, 38, 42, 43, 45, 61, 63, 64, 94, 95];
+  for (let i = 0; i < pwd.length; i++) {
+    let charcode = pwd.charCodeAt(i);
+    if (charcode >= 97 && charcode <= 122) lowercase++;
+    if (charcode >= 65 && charcode <= 90) uppercase++;
+    if (i !== 0 && (charcode >= 97 && charcode <= 122 || charcode >= 65 && charcode <= 90) && pwd[i] === pwd[i-1]) matchingAdjacentLetters = true;
+    if (charcode >= 48 && charcode <= 57) numbers++;
+    if (i !== 0 && charcode >= 48 && charcode <= 57 && pwd[i] === pwd[i-1]) matchingAdjacentNumbers = true;
+    if (charcode >= 0 )
+    if (allowedSymbolsCharCodes.includes(charcode)) {
+      symbolsCount++;
+      if (i !== pwd.length - 1 && pwd.includes(String.fromCharCode(charcode), i + 1)) symbolNotUnique = true;
+      if (i !== 0 && allowedSymbolsCharCodes.includes(pwd.charCodeAt(i - 1))) adjacentSymbols = true;
+    }
+    if (pwd[i] === '0') zero++;
+    if (pwd[i] === ' ') spaces++;
+  }
+  
+  pwd.length >= 16 ? document.getElementById('length').style.color = 'green' : document.getElementById('length').style.color = 'red';
+  (lowercase && uppercase) ? document.getElementById('capitalization').style.color = 'green' : document.getElementById('capitalization').style.color = 'red';
+  !matchingAdjacentLetters ? document.getElementById('adjacent-letters').style.color = 'green' : document.getElementById('adjacent-letters').style.color = 'red';
+  numbers >= 4 ? document.getElementById('min-numbers').style.color = 'green' : document.getElementById('min-numbers').style.color = 'red';
+  !matchingAdjacentNumbers ? document.getElementById('adjacent-numbers').style.color = 'green' : document.getElementById('adjacent-numbers').style.color = 'red';
+  symbolsCount >= 2 ? document.getElementById('min-symbols').style.color = 'green' : document.getElementById('min-symbols').style.color = 'red';
+  !symbolNotUnique && !adjacentSymbols ? document.getElementById('symbols-restraints').style.color = 'green' : document.getElementById('symbols-restraints').style.color = 'red';
+  !zero ? document.getElementById('zero-restraint').style.color = 'green' : document.getElementById('zero-restraint').style.color = 'red';
+  !spaces ? document.getElementById('spaces-restraint').style.color = 'green' : document.getElementById('spaces-restraint').style.color = 'red';
 }
-
-
 
 
 function loadPageListeners() {
   const input = document.getElementById('password');
-  input.addEventListener('input', validator);
+  input.addEventListener('input', debounce(validator, 250));
 }
 document.addEventListener('DOMContentLoaded', loadPageListeners);
